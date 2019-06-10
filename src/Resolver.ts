686e9f21a3d7e3f8626f92d93b590669534f9d84
LzxHahaha <lzxglhf@live.com>
2019-06-10 22:21:11 +0800

    add todo

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -18,17 +18,14 @@ export default class Resolver {
                 }
     
                 const args = path.node.arguments;
-                const dependencies = 
-                
-                
-                
-                
-                args[0] as t.ArrayExpression;
-                const define = args[1] as t.FunctionExpression;
+                const dependencies = args[0] as t.ArrayExpression;
                 
                 for (const node of dependencies.elements) {
                     state.dependencies.push((node as t.StringLiteral).value);
                 }
+
+                // TODO: analyze and load files first
+                // html process
 
                 path.traverse(self.bodyVisitor(), this);
             }
@@ -38,8 +35,24 @@ export default class Resolver {
     bodyVisitor() {
         return {
             FunctionExpression(path: NodePath<t.FunctionExpression>, state: any) {
-                console.log(state);
+                // TODO: get params, read config to ignore global modules
+                // goto return expression
+                // get mixins/componets etc. and merge mixin if used
             }
+        };
+    }
+
+    defineVisitor() {
+        return {
+            // get functions from config
+        };
+    }
+
+    functionVisitor() {
+        return {
+            // change this.data.xxx to this.xxx
+            // change life cycle, watch and computed
+            // save undefined fields from this.data.xxx (set to props)
         };
     }
 
