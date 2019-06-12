9b9c12f53af0222555b829dd3e97ae92cc0d6207
LzxHahaha <lzxglhf@live.com>
2019-06-12 20:51:33 +0800

    base resolver

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -1,134 +1,95 @@
-import * as t from 'babel-types';
-import traverse, { NodePath } from 'babel-traverse';
-import * as babylon from 'babylon';
+import { resolve as resolvePath, isAbsolute as isAbsolutePath } from 'path';
 
-enum ModuleType {
-    Unknown,
-    Object,
-    Component,
-    Template,
-    Style
-};
-
-class ModuleInfo {
-    loaded: boolean = false;
-    type: ModuleType = ModuleType.Unknown;
-    // TODO: module info fields
-
-    constructor(public path: string) {}
-}
+import Module, {
+    UnknownModule,
+    ObjectModule,
+    ComponentModule,
+    MixinModule,
+    TemplateModule,
+    StyleModule
+} from './modules';
 
 export default class Resolver {
-    paths: string[] = [];
-    modules = new Map<string, ModuleInfo>();
+    dependenceQueue: string[] = [];
 
-    constructor(public globalComponents = new Set<string>(), public globalModules = new Set<string>()) {}
+    constructor(
+        public basePath: string,
+        public globalComponents = new Set<string>(),
+        public globalModules = new Set<string>(),
+        public modules = new Map<string, Module>()
+    ) {}
 
-    start(code: string) {
-        const ast = babylon.parse(code);
-        traverse(ast, this.rootVisitor(), undefined, this);
+    async start(codePath: string): Promise<string> {
+        codePath = this.formatPath(codePath);
+        this.dependenceQueue.push(codePath);
+        while (this.dependenceQueue.length) {
+            const p = this.dependenceQueue.shift() as string;
+            await this.load(p);
+        }
+        return codePath;
     }
 
-    private loadModule(path: string) {
-        const module = this.modules.get(path);
-        if (!module || module.loaded) {
-            return;
+    /**
+     * Load single module.
+     * @param codePath Absolute path.
+     */
+    async load(codePath: string): Promise<Module> {
+        let module = this.modules.get(codePath);
+        if (!module) {
+            module = this.createModule(codePath);
+            this.modules.set(codePath, module);
+        }
+        if (module.loaded) {
+            return module;
+        }
+        await module.load();
+
+        // Regular的加载是文件级别，因此目录要往上找一级
+        const basePath = resolvePath(codePath, '..');
+        // 对组件和mixin再去加载依赖
+        if (module instanceof ComponentModule || module instanceof MixinModule) {
+            for (let modulePath of module.dependencies) {
+                modulePath = this.formatPath(modulePath, basePath);
+                if (!this.modules.has(modulePath)) {
+                    this.dependenceQueue.push(modulePath);
+                }
+            }
         }
 
-        switch (module.type) {
-            case ModuleType.Template:
-                // TODO: HTML resolver
-                break;
-            case ModuleType.Style:
-                // TODO: save style info
-                break;
-            case ModuleType.Component:
-                // TODO: resolve component
-                break;
-            default:
-                break;
+        return module;
+    }
+
+    /**
+     * Format path to absolute path.
+     * @param p Path.
+     */
+    private formatPath(p: string, basePath = this.basePath): string {
+        if (/^text!/.test(p)) {
+            p = p.substr(5);
         }
-        module.loaded = true;
+        if (isAbsolutePath(p)) {
+            return p;
+        }
+        return resolvePath(basePath, p);
     }
 
-    private rootVisitor() {
-        return {
-            CallExpression(path: NodePath<t.CallExpression>, state: Resolver) {
-                const callee = path.node.callee;
-                if (!t.isIdentifier(callee) || callee.name !== 'define') {
-                    return;
-                }
-    
-                const args = path.node.arguments;
-                const dependencies = args[0] as t.ArrayExpression;
-                
-                // 解析引入文件信息
-                for (const node of dependencies.elements) {
-                    let path = (node as t.StringLiteral).value;
-                    if (/^text!/.test(path)) {
-                        path = path.substr(5);
-                    }
-                    // TODO: 改为绝对路径
-
-                    state.paths.push(path);
-                    if (state.modules.has(path)) {
-                        return;
-                    }
-
-                    const module = new ModuleInfo(path);
-                    if (state.globalComponents.has(path)) {
-                        module.type = ModuleType.Component;
-                        module.loaded = true;
-                    } else if (state.globalModules.has(path)) {
-                        module.type = ModuleType.Object;
-                        module.loaded = true;
-                    } else if (/\.html$/.test(path)) {
-                        module.type = ModuleType.Template;
-                    } else if (/\.(css|less|sass|scss)/.test(path)) {
-                        module.type = ModuleType.Style;
-                    }
-                }
-
-                path.traverse(state.bodyVisitor(), this);
-                path.stop();
-            }
-        };
-    }
-
-    private bodyVisitor() {
-        return {
-            CallExpression(path: NodePath<t.CallExpression>, state: Resolver) {
-                const callee = path.node.callee;
-                if (!t.isMemberExpression(callee)) {
-                    return;
-                }
-                const { property, object } = callee;
-                if (!t.isIdentifier(property) || !t.isIdentifier(object)) {
-                    return;
-                }
-                console.log(property.name);
-                if (property.name === 'extend' && object.name) {}
-            },
-            FunctionExpression(path: NodePath<t.FunctionExpression>, state: any) {
-                console.log(path);
-                // TODO: get params, read config to ignore global modules
-                // goto return expression
-                // get mixins/componets etc. and merge mixin if used
-            }
-        };
-    }
-
-    private defineVisitor() {
-        return {
-            // get functions from config
-        };
-    }
-
-    private functionVisitor() {
-        return {
-            // change this.data.xxx to this.xxx
-            // change life cycle, watch and computed
-            // save undefined fields from this.data.xxx (set to props)
-        };
+    /**
+     * Create a module.
+     * @param modulePath Absolute path.
+     */
+    private createModule(modulePath: string): Module {
+        let module: Module;
+        if (this.globalComponents.has(modulePath)) {
+            module = new ComponentModule(modulePath, true);
+        } else if (this.globalModules.has(modulePath)) {
+            module = new ObjectModule(modulePath);
+        } else if (/\.html$/.test(modulePath)) {
+            module = new TemplateModule(modulePath);
+        } else if (/\.(css|less|sass|scss)/.test(modulePath)) {
+            module = new StyleModule(modulePath);
+        } else {
+            module = new UnknownModule(modulePath);
+        }
+        return module;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import fs from 'fs';
 import path from 'path';
 
 import Resolver from './Resolver';
 
-const filePath = path.resolve(__dirname, '../resources/test.js');
-const code = fs.readFileSync(filePath).toString();
+const basePath = path.resolve(__dirname, '../resources');
 
-const resolver = new Resolver(new Set(['pro/BaseComponent']), new Set(['pro/util']));
-resolver.start(code);
-console.log(resolver);
+const resolver = new Resolver(
+    basePath,
+    new Set(['pro/BaseComponent']),
+    new Set(['pro/util'])
+);
+const entry = resolver.start('./index.js');
+
+console.log(entry);
